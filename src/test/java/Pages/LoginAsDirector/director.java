ea6bedfdb392e4b4d14788b48f1f02dbf909649c
shrey-deloitte <shredeshpande@deloitte.com>
2022-04-21 11:21:16 +0530

    Amended changes in framework

---
+++ src/test/java/Pages/LoginAsDirector/director.java
@@ -0,0 +1,7 @@
+package Pages.LoginAsDirector;
+
+public class director {
+    public static void main(String[] args) {
+        System.out.println("jbd");
+    }
+}
---
+++ src/test/java/Pages/LoginAsLinker/linker.java
@@ -0,0 +1,7 @@
+package Pages.LoginAsLinker;
+
+public class linker {
+    public static void main(String[] args) {
+        System.out.println("shgd");
+    }
+}
