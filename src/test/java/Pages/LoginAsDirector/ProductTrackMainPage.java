5e1453af567356110c64b27da265ce30db56d0ee
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-04-22 18:27:23 +0530

    sample links and download verified

--- src/test/java/Pages/LoginAsDirector/ProductTrackMainPage.java
+++ src/test/java/Pages/LoginAsDirector/ProductTrackMainPage.java
@@ -1,25 +1,32 @@
 package Pages.LoginAsDirector;
 
+import org.apache.xmlbeans.GDuration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import java.time.Duration;
+import java.io.File;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class ProductTrackMainPage {
 
     static WebDriver driver;
+
     //Xpaths
     By productTrack =By.xpath("//div[@id='rc-tabs-0-tab-2']");
     String productTrackString="//div[@id='rc-tabs-0-tab-2']";
     By upoloadTypes = By.xpath("//div[contains(@class, 'uploader')]");
-    By downloadSampleLink= By.xpath("//div[contains(@class, 'uploader')]/child::div[1]");
+    By downloadSampleLink= By.xpath("//div[@class='head']/child::a");
 
     //constructor
     public ProductTrackMainPage(WebDriver driver) {
@@ -32,8 +39,32 @@ public class ProductTrackMainPage {
         List<WebElement> numberOfUploadTypes =driver.findElements(upoloadTypes);
         Assert.assertEquals(numberOfUploadTypes.size(),5);
     }
-    public void verifyDownloadSampleLink(){
-        List<WebElement> sampleLinks= driver.findElements(downloadSampleLink);
-        System.out.println(sampleLinks.size());
+    public void verifyFilesDownloadedOrNot() throws InterruptedException {
+        List<WebElement> sampleLinks = driver.findElements(downloadSampleLink);
+        ArrayList<String> targets=new ArrayList<String>();
+
+        for(WebElement link:sampleLinks){
+            targets.add(link.getAttribute("href"));
+        }
+        for(String target:targets){
+            driver.navigate().to(target);
+            Thread.sleep(500);
+        }
+        ArrayList<String> filenames= new ArrayList<>();
+        filenames.add("linkerdata (1).csv");
+        filenames.add("trackresults (2).csv");
+        filenames.add("productdetail (1).csv");
+        filenames.add("sectionlead (1).csv");
+        filenames.add("productpref (1).csv");
+
+        File location = new File("C:\\Users\\shubhamkumar32\\Downloads");
+        File [] totalFiles = location.listFiles();
+
+        for(File file:totalFiles){
+            if(filenames.contains(file.getName())){
+                Assert.assertTrue(true);
+            }
+        }
     }
+
 }
--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -3,28 +3,65 @@ package Tests;
 import Pages.LoginAsDirector.LoginPage;
 import Pages.LoginAsDirector.ProductTrackMainPage;
 import net.bytebuddy.build.Plugin;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Tests {
     WebDriver driver;
+    File folder;
     String HomeUrl="https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/";
     LoginPage loginPage;
     ProductTrackMainPage productTrackMainPage;
 
     @BeforeSuite
-    public void setup() throws InterruptedException {
+    public void fileSetup(){
+        folder = new File(UUID.randomUUID().toString());
+        folder.mkdir();
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+
+//        ChromeOptions options= new ChromeOptions();
+//        Map<String,Object> prefs= new HashMap<String, Object>();
+//        prefs.put("profile.default_content_settings.popups",0);
+//        prefs.put("download.default_directory",folder.getAbsolutePath());
+//
+//        options.setExperimentalOption("prefs",prefs);
+//        DesiredCapabilities caps = DesiredCapabilities.chrome();
+//        caps.setCapability(ChromeOptions.CAPABILITY,options);
+//        options.addArguments("--start-maximized");
+//        options.merge(caps);
+
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(HomeUrl);
 
 
+
     }
+//    @BeforeSuite
+//    public void setup() throws InterruptedException {
+//
+//
+//
+//
+//    }
     @Test(priority = 1)
     public void loginAvatarClick() throws InterruptedException {
        loginPage =new LoginPage(driver);
@@ -47,10 +84,26 @@ public class Tests {
         productTrackMainPage=new ProductTrackMainPage(driver);
         productTrackMainPage.verifyNumberOfUploadTypes();
     }
+//    @Test(priority = 5)
+//    public void verifyDownloadSampleLink(){
+//        productTrackMainPage=new ProductTrackMainPage(driver);
+//        productTrackMainPage.verifyDownloadSampleLink();
+//    }
     @Test(priority = 5)
-    public void verifyDownloadSampleLink(){
+    public void download() throws InterruptedException {
+
         productTrackMainPage=new ProductTrackMainPage(driver);
-        productTrackMainPage.verifyDownloadSampleLink();
+        productTrackMainPage.verifyFilesDownloadedOrNot();
+
     }
 
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+
+        for(File file:folder.listFiles()){
+            file.delete();
+        }
+        folder.delete();
+    }
 }
