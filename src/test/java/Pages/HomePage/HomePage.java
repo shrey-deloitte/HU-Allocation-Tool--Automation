14be9c154b63a27f90e4d2769b373f87ae0762c0
suhasm-deloitte <suhasm@deloitte.com>
2022-04-22 19:17:41 +0530

    Checking GetStarted Button and title

--- src/test/java/Pages/HomePage/HomePage.java
+++ src/test/java/Pages/HomePage/HomePage.java
@@ -4,35 +4,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
 
     //Initializing web driver
-    static WebDriver driver;
-    WebElement ele = driver.findElement(By.xpath("//button[@class='btnstart performbtn btn btn-primary btn-lg']"));
-    By Get_started_btn = By.xpath("//*[@id=\"root\"]/div/div[3]/div[1]/div[2]/button");
-    By cancel = By.xpath("/html/body/div/div/div[1]/button[1]");
-    By left = By.xpath("//span[@class=\"carousel-control-prev-icon\"]");
-    By right = By.xpath("//span[@class=\"carousel-control-next-icon\"]");
-    String url = "https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/";
+   static By ele = By.xpath("//button[@class='btnstart performbtn btn btn-primary btn-lg']");
+   static By cancel = By.xpath("/html/body/div/div/div[1]/button[1]");
+   static By left = By.xpath("//span[@class=\"carousel-control-prev-icon\"]");
+   static By right = By.xpath("//span[@class=\"carousel-control-next-icon\"]");
+   static String url = "https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/";
 
-
-    //Home Page
-    public HomePage(WebDriver driver){
-        this.driver = driver;
-    }
 
     //Open Page
-    public void open_page() throws InterruptedException {
-        driver.manage().window().maximize();
+    public static WebDriver open_page() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suhasm\\Downloads\\selenium\\selenium 4.1.2\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get(url);
-        Thread.sleep(1000);
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        return driver;
     }
 
-    public String title() throws InterruptedException{
+    public static String title(WebDriver driver){
        return driver.getTitle();
     }
-    public void carousel() throws InterruptedException {
+    public static void carousel(WebDriver driver) throws InterruptedException {
         driver.findElement(left).click();
         Thread.sleep(2000);
 
@@ -40,14 +37,13 @@ public class HomePage {
         Thread.sleep(2000);
     }
 
-    public void GetStarted_btn() throws InterruptedException {
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("arguments[0].click()", ele);
+    public static void GetStarted_btn(WebDriver driver) throws InterruptedException {
+        WebElement Get_started_btn = driver.findElement(ele);
+//        JavascriptExecutor jse = (JavascriptExecutor) driver;
+//        jse.executeScript("arguments[0].click()", Get_started_btn);
+        Get_started_btn.click();
+        Thread.sleep(2000);
         driver.findElement(cancel).click();
         Thread.sleep(2000);
-    }
-
-    public void Team_members_info(){
-
     }
 }
--- src/test/java/Tests/Test_Homepage.java
+++ src/test/java/Tests/Test_Homepage.java
@@ -1,87 +1,57 @@
 package Tests;
 import Pages.HomePage.HomePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 
 public class Test_Homepage {
 
     WebDriver driver;
-    HomePage home;
     String title_to_check = "HU ALLOCATION TOOL";
     String url = "https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/";
 
     //Initializing web driver
     @BeforeTest
     public void setup() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suhasm\\Downloads\\selenium\\selenium 4.1.2\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get(url);
-        driver.manage().window().maximize();
+        driver = HomePage.open_page();
         Thread.sleep(1000);
     }
 
     //Checking Title
     @Test(priority = 1)
     public void Check_Title() throws InterruptedException {
-        home = new HomePage(this.driver);
-        String Title = home.title();
         Thread.sleep(2000);
-        if (Title == title_to_check) {
-            System.out.println("PASS");
-        } else {
-            System.out.println("FAIL");
+        try {
+            Thread.sleep(2000);
+            String title = HomePage.title(driver);
+            Assert.assertEquals(title,title_to_check);
+            System.out.println("Title is matched, title is "+title);
+        }
+        catch (Exception e){
+            System.out.println("Title is not matched");
         }
     }
 
     //Checking Carousel
-    @Test(priority = 2)
-    public void Carousel() throws InterruptedException {
-        home = new HomePage(driver);
-        home.carousel();
+    @Test(priority = 1)
+    void Carousel() throws InterruptedException {
+        HomePage.carousel(driver);
         Thread.sleep(1000);
     }
 
     //Checking 'Get Started Button
-    @Test(priority = 3)
-    public void GetStarted_Btn() throws InterruptedException {
-        home = new HomePage(driver);
-        home.GetStarted_btn();
+    @Test(priority = 2)
+    void GetStarted_Btn() throws InterruptedException {
+        HomePage.GetStarted_btn(driver);
         Thread.sleep(2000);
     }
 
-    //
-
-
     @AfterTest
-    public void Close_Browser() {
+    void Close_Browser() {
         driver.close();
     }
-
-
 }
-
-
-        /*@Test
-        public void main() throws InterruptedException {
-
-    System.setProperty("webdriver.chrome.driver","C:\\Users\\suhasm\\Downloads\\selenium\\selenium 4.1.2\\chromedriver\\chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/");
-    driver.manage().window().maximize();
-
-    Thread.sleep(2000);
-    driver.findElement(By.xpath("//span[@class=\"carousel-control-prev-icon\"]")).click();
-    Thread.sleep(2000);
-    driver.findElement(By.xpath("//span[@class=\"carousel-control-next-icon\"]")).click();
-    Thread.sleep(2000);
-    driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div[1]/div[2]/button")).click();
- // driver.findElement(By.xpath("//button[@class='btnstart btn btn-primary btn-lg']")).click();
-    Thread.sleep(2000);
-    driver.findElement(By.xpath("//button[@class=\"ant-btn ant-btn-default\"]")).click();
-    Thread.sleep(2000);*/
-
-            //driver.close();
