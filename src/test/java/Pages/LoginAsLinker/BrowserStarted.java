a0680330b77dbda8df069031640fb580c25b3d6e
Priya <shampriya@deloitte.com>
2022-04-23 01:25:59 +0530

    login & preference

--- src/test/java/Pages/LoginAsLinker/BrowserStarted.java
+++ src/test/java/Pages/LoginAsLinker/BrowserStarted.java
@@ -1,28 +1,32 @@
 package Pages.LoginAsLinker;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 import java.time.Duration;
+
 public class BrowserStarted {
+    LinkersLogin linkersLogin;
+    static WebDriver driver;
     String appURL = "https://automatedhuallocation-ui-urtjok3rza-wl.a.run.app/";
     String GetStarted = "//*[@id=\"root\"]/div/div[3]/div[1]/div[2]/button";
-    String Username = "//*[@id=\"basic_username\"]";
-    String Password = "//*[@id=\"basic_password\"]";
-    String Submit = "//*[@id=\"basic\"]/div[3]/div/div/div/button";
     String Notification = "//*[@id=\"root\"]/div/div[1]/div[3]/div[1]";
-    public WebDriver driver;
 
     @Test(priority = 1)
-    public void setup() {
+    public void setup() throws IOException {
         System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
-        driver.get(appURL);}
+        driver.get(appURL);
+        linkersLogin = new LinkersLogin(driver);
+    }
+
     @Test(priority = 2)
     public void linkerLogin() {
         WebElement element = driver.findElement(By.xpath(GetStarted));
@@ -30,39 +34,40 @@ public class BrowserStarted {
         js.executeScript("arguments[0].click();", element);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-        driver.findElement(By.xpath(Username)).sendKeys("gheavens2");
-        driver.findElement(By.xpath(Password)).sendKeys("password");
-        driver.findElement(By.xpath(Submit)).click();
     }
+
     @Test(priority = 3)
+    public void Login() throws InterruptedException {
+        linkersLogin.Login();
+    }
+
+    @Test(priority = 5)
     public void getNotify() throws InterruptedException {
         driver.findElement(By.xpath(Notification)).click();
         driver.findElement(By.xpath(Notification)).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-    @Test(priority = 4)
-    public void preferenceButton(){
-        driver.findElement(By.xpath("//*[contains(text(),'Preference Form')]")).click();
 
+    @Test(priority = 6)
+    public void preferenceButton() throws InterruptedException {
+        driver.findElement(By.xpath("//*[contains(text(),'Preference Form')]")).click();
+        driver.findElement(By.xpath("//*[@id=\"vertical-tabpanel-0\"]/div/p/button")).click();
+        //driver.findElement(By.name("Fill Your Preference")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[contains(text(),'Data Engineering')]")).click();
+    }
+
+    @Test(priority = 7)
+    public void Submit() {
+        driver.findElement(By.xpath("//*[@id=\"vertical-tabpanel-0\"]/div/p/div[2]/button")).click();
+        System.out.println("Submit is clicked but not working");
+
+    }
+
+    @AfterTest
+    public void AppQuit() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.close();
     }
 }
-//    }
-//    public void Login(WebDriver driver) {
-//        this.driver = driver;
-//    }
-//    @Test
-//    public void login(){
-//        ReadingExcel readingExcel = new ReadingExcel();
-//     //   driver.findElement(By.xpath(Username)).sendKeys(readingExcel.Username);
-//        System.out.println(readingExcel.Username);
-//     //   driver.findElement(By.xpath(Password)).sendKeys(readingExcel.Password);
-//        System.out.println(readingExcel.Password);
-      //  driver.findElement(By.xpath(Submit)).click();
-
-//
-//    @AfterTest
-//   public void AppQuit(){
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-//       driver.close();
-//}
 
--- src/test/java/Pages/LoginAsLinker/ReadingExcel.java
+++ src/test/java/Pages/LoginAsLinker/ReadingExcel.java
@@ -1,44 +1,40 @@
-//package Pages.LoginAsLinker;
-//import org.apache.poi.xssf.usermodel.XSSFRow;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//import org.testng.annotations.Test;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//
-//
-//
-//public class ReadingExcel {
-//    public String Username;
-//    public String Password;
-//
-//    @Test
-//    public void readCells() throws IOException {
-//        String excelFilePath = "src\\test\\resources\\LinkersLogin.xlsx";
-//        FileInputStream input = new FileInputStream(excelFilePath);
-//        XSSFWorkbook wb = new XSSFWorkbook(input);
-//        XSSFSheet sheet = wb.getSheetAt(0);
-//        int rows = sheet.getLastRowNum();
-//        int cols = sheet.getRow(1).getLastCellNum();
-//        for (int i = 1; i <= rows; i++) {
-//            XSSFRow row = sheet.getRow(i);
-//            for (int j = 0; j < cols; j++) {
-//                XSSFRow cell = sheet.getRow(j);
-//                if (j == 0) {
-//                    Username = row.getCell(0).getStringCellValue();
-//                }
-//                if (j == 1) {
-//                    Password = String.valueOf(cell.getCell(j));
-//                }
-//            }
-//        }
-//        System.out.println(Username + " " + Password);
-//    }
-//}
-
-//    public void User(String name, String email, String password, int age) {
-//        this.Username = name;
-//        this.Password = password;
-//    }
-//}
+package Pages.LoginAsLinker;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.IOException;
+public class ReadingExcel {
+    public static String Username;
+    public static String Password;
+@Test
+    public  void readCells() throws IOException {
+        String excelFilePath = "src\\test\\resources\\LinkersLogin.xlsx";
+        FileInputStream input = new FileInputStream(excelFilePath);
+        XSSFWorkbook wb = new XSSFWorkbook(input);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(1).getLastCellNum();
+        for (int i = 1; i <= rows; i++) {
+            XSSFRow row = sheet.getRow(i);
+            for (int j = 0; j < cols; j++) {
+                XSSFCell cell = row.getCell(j);
+                if (j == 0) {
+                    Username = cell.getStringCellValue();
+                }
+                if (j == 1) {
+                    Password = cell.getStringCellValue();
+                }
+            }
+        }
+        System.out.println(Username + " " + Password);
+    }
+    public static String getUsername(){
+        return Username;
+    }
+    public static String getPassword(){
+        return Password;
+    }
+}
