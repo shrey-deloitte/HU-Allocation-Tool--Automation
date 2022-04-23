accc366b3849b2c7dd042775f21e597eb662b171
Priya <shampriya@deloitte.com>
2022-04-23 15:32:21 +0530

    login & notification

--- src/test/java/Pages/LoginAsLinker/ExtentHtmlReports.java
+++
@@ -1,8 +0,0 @@
-package Pages.LoginAsLinker;
-
-import com.aventstack.extentreports.observer.ExtentObserver;
-
-public class ExtentHtmlReports implements ExtentObserver {
-    public ExtentHtmlReports(String s) {
-    }
-}
